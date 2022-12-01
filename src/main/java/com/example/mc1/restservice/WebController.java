587c839b362d4b3f7ab902f50ae723e710b8a476
Sergey Stepurko <82845341+SStepurko@users.noreply.github.com>
2022-12-01 14:47:05 +0500

    Update WebController.java
    Run process starter and stopper on GET endpoints

--- src/main/java/com/example/mc1/restservice/WebController.java
+++ src/main/java/com/example/mc1/restservice/WebController.java
@@ -1,5 +1,6 @@
 package com.example.mc1.restservice;
 
+import com.example.mc1.service.ProcessController;
 import com.example.mc1.websocket.SendWebSocketMessage;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,16 +10,19 @@ import org.springframework.web.bind.annotation.*;
 public class WebController {
 
 	private final MessageService service;
+	private final ProcessController controller;
 
-	public WebController(MessageService service) {
+	public WebController(MessageService service, ProcessController controller) {
 		this.service = service;
+		this.controller = controller;
 	}
 
 	@GetMapping(value = "/start/")
 	public ResponseEntity<String> startSending() {
 		try {
-			Message message = service.generateMessage();
-			SendWebSocketMessage.sendWebSocket(message);
+			controller.setRunTimerTask();
+//			Message message = service.generateMessage();
+//			SendWebSocketMessage.sendWebSocket(message);
 			return ResponseEntity.ok("Started");
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body("Bad request");
@@ -28,6 +32,7 @@ public class WebController {
 	@GetMapping(value = "/stop/")
 	public ResponseEntity<String> stopSending() {
 		try {
+			controller.stopRunTimerTask();
 			return ResponseEntity.ok("Stopped");
 		} catch (Exception e) {
 			return ResponseEntity.badRequest().body("Bad request");
