5f23ba0cb8497f0403229c51e26d4073388caf06
Sergey Stepurko <82845341+SStepurko@users.noreply.github.com>
2022-12-02 20:19:11 +0500

    Update ProcessController.java
    Changed to services. Now works multiply start stop.

--- src/main/java/com/example/mc1/service/ProcessController.java
+++ src/main/java/com/example/mc1/service/ProcessController.java
@@ -5,113 +5,100 @@ import com.example.mc1.restservice.MessageService;
 import com.example.mc1.websocket.SendWebSocketMessage;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.*;
 
+/**
+ * Starts and stops process of sending.
+ * New start command will stop execution of old one.
+ * Prints result at the end or when stopped
+ */
 @Component
 public class ProcessController {
 
-	private static boolean isRunning;
-	private final MessageService service;
-	private final ConfigReader config;
-	Timer timer = new Timer();
+	//	Services for stop and start sending
+	static ExecutorService service;
+	static ScheduledExecutorService timerService;
+	static Future<?> currentJob;
+	static ScheduledFuture<?> timerJob;
 
-	MyThread runThread = new MyThread();
-	TimerTask stopTimerTask = new TimerTask() {
-		@Override
-		public void run() {
-			isRunning = false;
-			Results.stopCount();
-			runThread.pause();
-			timer.cancel();
-			timer.purge();
-		}
-	};
+	//	injections
+	private final MessageService messageService;
+	private final ConfigReader configReader;
 
-	public ProcessController(MessageService service, ConfigReader config) {
-		this.service = service;
-		this.config = config;
-		isRunning = false;
+	public ProcessController(MessageService messageService, ConfigReader configReader) {
+		this.messageService = messageService;
+		this.configReader = configReader;
 	}
-
-	private void startRunTimerTask(long delay, long period) {
-		Results.startCount();
-		if (runThread.isWasStarted()) {
-			runThread.resumeIt();
-		} else {
-			runThread.start();
-		}
-
-		Timer timer = new Timer();
-		timer.schedule(stopTimerTask, delay);
-	}
-
-	public void setRunTimerTask() {
-//		do nothing if running
-		if (isRunning) {
-			return;
-		}
-//		get from config
-		Map<String, Long> confMap = new HashMap<>();
-		confMap = config.getProperties();
-		startRunTimerTask(confMap.get(config.getRuntimeKey()), confMap.get(config.getPeriodKey()));
-	}
-
-	public void stopRunTimerTask() {
-		if (isRunning) {
-			timer.cancel();
-			timer.purge();
-			timer = null;
-			runThread.pause();
-			Results.stopCount();
-			isRunning = false;
-		}
-	}
-
 
 	/**
-	 * thread with pause and stop
+	 * Stops all current processes and cancel scheduled.
+	 * Prints results
 	 */
-	class MyThread extends Thread {
-		private boolean run;
-		private boolean wasStarted;
+	private static void stopALL() {
+		Results.stopCount();
+		if (currentJob != null && !currentJob.isDone()) currentJob.cancel(true);
+		if (timerJob != null && !timerJob.isDone()) timerJob.cancel(true);
+	}
 
-		public MyThread() {
+	/**
+	 * Starts sending process and schedule stop. New call of method will end running old one.
+	 * Starts counting messages and time for Result class.
+	 * Prints results.
+	 *
+	 * @param periodMs frequency of sending messages in milliseconds
+	 * @param delayMs  period of sending messages in milliseconds
+	 */
+	private void startTreadAndTimer(long periodMs, long delayMs) {
+// check if executors exist and create new if don't
+		if (service == null) service = Executors.newFixedThreadPool(1);
+		if (timerService == null) timerService = Executors.newScheduledThreadPool(1);
+
+//		check if process was running - stop it and print results
+		if (currentJob != null && !currentJob.isDone()) {
+			Results.stopCount();
+			currentJob.cancel(true);
 		}
-
-		@Override
-		public void run() {
-			this.run = true;
-			while (run) {
-				isRunning = true;
-				Message message = service.generateMessage();
+//		start sending process
+		currentJob = service.submit(() -> {
+			Results.startCount();
+			while (true) {
+				Message message = messageService.generateMessage();
 				SendWebSocketMessage.sendWebSocket(message);
 				Results.countMessage();
+				System.out.println("task is running");
+				Thread.sleep(periodMs);
 			}
-		}
+		});
 
-		@Override
-		public synchronized void start() {
-			this.setWasStarted(true);
-			super.start();
-		}
+//		check if there was schedule and cancel it
+		if (timerJob != null && !timerJob.isDone()) timerJob.cancel(true);
+//		set new schedule
+		timerJob = timerService.schedule(() -> {
+			Results.stopCount();
+			if (currentJob != null && !currentJob.isDone()) currentJob.cancel(true);
+		}, delayMs, TimeUnit.SECONDS);
+	}
 
-		public void pause() {
-			this.run = false;
-		}
+	/**
+	 * Starts private method for sending messages. Gets parameters from config file.
+	 * New call of method will end running old one and print results
+	 * Starts counting messages and time for Result class.
+	 */
+	public void setRunTimerTask() {
+//		get from config
+		Map<String, Long> confMap;
+		confMap = configReader.getProperties();
+		long periodMs = confMap.get(configReader.getPeriodKey());
+		long delayMs = confMap.get(configReader.getRuntimeKey());
+//		start process
+		this.startTreadAndTimer(periodMs, delayMs);
+	}
 
-		public void resumeIt() {
-			this.run = true;
-		}
-
-		public boolean isWasStarted() {
-			return wasStarted;
-		}
-
-		public void setWasStarted(boolean wasStarted) {
-			this.wasStarted = wasStarted;
-		}
+	/**
+	 * Stop sending processes and print results.
+	 */
+	public void stopRunTimerTask() {
+		stopALL();
 	}
 }
